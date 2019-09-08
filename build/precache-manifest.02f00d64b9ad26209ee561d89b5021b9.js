c35dfaf0234cc73c318d67da41c4e8af8f0d0019
luizkrlz <luiz.correa.dev@gmail.com>
2019-09-07 21:34:25 -0300

    deploy

---
+++ build/precache-manifest.02f00d64b9ad26209ee561d89b5021b9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47cc22c0dcb1cfdeb97b5f5bdfefa767",
+    "url": "/index.html"
+  },
+  {
+    "revision": "958299f0884261b33244",
+    "url": "/static/js/2.ccb62c0a.chunk.js"
+  },
+  {
+    "revision": "371f7517ab63926e291c",
+    "url": "/static/js/main.7e60ccf3.chunk.js"
+  },
+  {
+    "revision": "d463d4784e9bafa1c236",
+    "url": "/static/js/runtime~main.ff555228.js"
+  }
+]);
